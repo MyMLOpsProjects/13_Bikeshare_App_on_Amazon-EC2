0fd2c63711453f6c3bdf8c13a1d308d42416ac3b
Pankaj Soni <90625369+pycsr@users.noreply.github.com>
2024-05-08 16:05:37 +0530

    Update main.py

--- bikeshare_model_api/app/main.py
+++ bikeshare_model_api/app/main.py
@@ -55,4 +55,4 @@ if settings.BACKEND_CORS_ORIGINS:
 if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
